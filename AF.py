7f49dcd3ba2c9d8fda1812ecdc4a332088edfe8b
Archana <archanalife7780@gmail.com>
2021-04-09 21:59:08 +0530

    the code is clear..

--- AF.py
+++ AF.py
@@ -1,15 +1,15 @@
 #------arithmetic function are
 num1=eval(input('enter a number'))
 num2=eval(input('enter a number'))
-
-print(sum(num1,num2))
+sum=num1+num2
 diff=num1-num2
 mul=num1*num2
 div=num1/num2
 
+print(sum)
 print(diff)
 print(mul)
 print(div)
 
 
-Thank you
+print('Thankyou')
